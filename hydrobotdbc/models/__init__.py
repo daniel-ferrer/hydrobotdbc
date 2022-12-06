653cf4062573d186ed69fddd71a4b0c26720a156
Daniel Ferrer <ferrdan2506@hotmail.com>
2022-12-06 14:45:59 -0600

    added UserSession model

--- hydrobotdbc/models/__init__.py
+++ hydrobotdbc/models/__init__.py
@@ -2,3 +2,4 @@ from .user import User
 from .collection import Collection
 from .user_birthday import UserBirthday
 from .user_voicestate import UserVoiceState
+from .user_session import UserSession

b98583d62dcb87a85dd034c80689a7b05001ec52
Daniel Ferrer <ferrdan2506@hotmail.com>
2023-03-13 17:44:39 -0500

    published version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
    name='hydrobotdbc',
-   version='0.0.10',
+   version='0.0.11',
    description='A custom Pyodbc wrapper',
    long_description=long_description,
    long_description_content_type="text/markdown",

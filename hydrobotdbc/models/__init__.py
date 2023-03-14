1efe17389a316b3f744a590cb7afd1074ae5dd01
Daniel Ferrer <ferrdan2506@hotmail.com>
2023-03-14 18:54:07 -0500

    added UserSeed import

--- hydrobotdbc/models/__init__.py
+++ hydrobotdbc/models/__init__.py
@@ -3,6 +3,7 @@ from .game import Game
 from .user import User
 from .poll import Poll
 from .wallet import Wallet
+from .user_seed import UserSeed
 from .collection import Collection
 from .user_wallet import UserWallet
 from .daily_claim import DailyClaim

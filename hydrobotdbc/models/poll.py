da10b5cd8781dec1b718dc4fc1124e80f604c341
isaacdll23 <isaacdelalama1013@gmail.com>
2023-03-20 23:15:13 -0500

    updated poll model

--- hydrobotdbc/models/poll.py
+++ hydrobotdbc/models/poll.py
@@ -3,22 +3,25 @@ from .collection import Collection
 
 
 class Poll:
-    __tablename__ = 'PollsQueue'
+    __tablename__ = 'Polls'
 
     class Query:
         def __init__(self):
             self.client = Client()
 
-        def get(self):
-            row = self.client.exec_fetchone(f"SELECT * FROM PollsQueue WHERE PollId={id}")
+        def get(self, poll_id):
+            row = self.client.exec_fetchone(f"SELECT * FROM Polls WHERE PollId={poll_id}")
 
             return None if row is None else Poll(row.DiscordId, row.Title, row.Options, row.PollId, row.DateRecAdded)
 
-        def filter_by(self, discord_id=None):
+        def filter_by(self, discord_id=None, completed=None):
             sql = "SELECT * FROM PollsQueue "
 
+            allow_multi_clause = False
             if discord_id is not None:
                 sql += f"WHERE DiscordId={discord_id}"
+            if completed is not None:
+                sql += f"AND Completed='{completed}' " if allow_multi_clause else f"WHERE Completed='{completed}' "
 
             sql += "ORDER BY DateRecAdded ASC"
 
@@ -26,17 +29,26 @@ class Poll:
 
             polls = []
             for row in rows:
-                polls.append(Poll(row.DiscordId, row.Title, row.Options, row.PollId, row.DateRecAdded))
+                polls.append(Poll(poll_id=row.PollId, discord_id=row.DiscordId, title=row.Title,
+                                  options=row.Options, winning_options=row.WinningOptions,
+                                  winner_votes=row.WinnerVotes, total_votes=row.TotalVotes,
+                                  poll_duration=row.PollDuration, completed=row.Completed,
+                                  date_rec_added=row.DateRecAdded))
 
             return Collection(polls)
 
     query = Query()
 
-    def __init__(self, discord_id, title, options, poll_id=None,  date_rec_added=None):
+    def __init__(self, discord_id, title, options, winning_options, winner_votes, total_votes, poll_duration, completed, poll_id=None, date_rec_added=None):
         self.PollId = poll_id
         self.DiscordId = discord_id
         self.Title = title
         self.Options = options
+        self.WinningOptions = winning_options
+        self.WinnerVotes = winner_votes
+        self.TotalVotes = total_votes
+        self.PollDuration = poll_duration
+        self.Completed = completed
         self.DateRecAdded = date_rec_added
 
     @property
@@ -54,6 +66,26 @@ class Poll:
     @property
     def options(self):
         return self.Options
+
+    @property
+    def winningOptions(self):
+        return self.WinningOptions
+
+    @property
+    def winnerVotes(self):
+        return self.WinnerVotes
+
+    @property
+    def totalVotes(self):
+        return self.TotalVotes
+
+    @property
+    def pollDuration(self):
+        return self.PollDuration
+
+    @property
+    def completed(self):
+        return self.Completed
 
     @property
     def date_rec_added(self):
